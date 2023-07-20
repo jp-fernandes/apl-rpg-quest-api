ed3365e9272846f8a76a9bd262d357a11a77fd6f
Joao Paulo Fernandes <joao.paulo.f.souza@accenture.com>
2023-07-20 18:44:08 -0300

    novo endpoint de getFeedback

--- src/routes/feedback.js
+++ src/routes/feedback.js
@@ -2,6 +2,17 @@ const express = require("express");
 const router = express.Router();
 const feedbackService = require("../services/feedbackService");
 
+router.get("/all", async (req, res) => {
+  try {
+    const feedbacks = await feedbackService.getAllFeedbacks();
+    res.json(feedbacks);
+  } catch (error) {
+    const errorMessage = error.message || "Erro ao obter os feedbacks";
+    const errorCode = error.code || 500;
+    res.status(errorCode).json({ error: errorMessage, code: errorCode });
+  }
+});
+
 router.post("/", async (req, res) => {
   try {
     const feedback = await feedbackService.saveFeedback(req);
--- src/services/feedbackService.js
+++ src/services/feedbackService.js
@@ -4,6 +4,34 @@ const moment = require("moment-timezone");
 const currentDate = moment().tz("America/Sao_Paulo");
 const { getOperatingSystem } = require("../utils/utils");
 
+async function getAllFeedbacks() {
+  try {
+    const snapshot = await feedbackCollectionDB.get();
+
+    const feedbacks = [];
+
+    snapshot.forEach((doc) => {
+      const feedbackData = doc.data();
+      const feedback = {
+        feedbackId: doc.id,
+        rating: feedbackData.rating,
+        suggestion: feedbackData.suggestion,
+        name: feedbackData.name,
+        surname: feedbackData.surname,
+        email: feedbackData.email,
+        createdDate: feedbackData.createdDate.toDate().toISOString(),
+        createdHour: feedbackData.createdHour,
+        operatingSystem: feedbackData.operatingSystem,
+      };
+      feedbacks.push(feedback);
+    });
+
+    return feedbacks;
+  } catch (error) {
+    throw error || new Error("Erro ao obter os feedbacks");
+  }
+}
+
 async function saveFeedback(req) {
   const userAgent = req.headers["user-agent"];
   const operatingSystem = getOperatingSystem(userAgent);
@@ -33,5 +61,6 @@ async function saveFeedback(req) {
 }
 
 module.exports = {
+  getAllFeedbacks,
   saveFeedback,
 };
